f8e96ec10bdd282f83b8596709ec0cd9e01730a8
karen <karen@gmail.com>
2026-03-08 14:28:39 +0800

    新增動態區間提示

--- guess_game.py
+++ guess_game.py
@@ -8,7 +8,7 @@ Created on Mon Jan 26 20:08:05 2026
 import random
 
 start,end=1,100
-count=3
+count=10
 
 # 產生亂數
 x=random.randint(start, end)    
@@ -18,16 +18,29 @@ x=random.randint(start, end)
 for i in range(count):
     y=int(input(f"第{i+1}/{count}次，請猜一個數字({start}~{end}):"))
 
+    if y<start or y>end:
+        print("不要亂猜~~~!")
+        continue
     
     # 猜對 + break
     if x==y:
         print("恭喜猜對!")
         break
     
+    # x=25
+    # y=10
+
+
     # 猜錯
     if x>y:
+        if y>start:
+            start=y+1
+
         print("猜大一點")
     else:
+        if y<end:
+            end=y-1
+
         print("猜小一點")
    
 
